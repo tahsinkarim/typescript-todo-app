0f334b105851f5397867e946756cb76de3336c1a
tahsinkarim <tahsinnkarim@gmail.com>
2022-10-19 20:13:45 +0600

    Updated Style

--- src/App.tsx
+++ src/App.tsx
@@ -32,18 +32,65 @@ const App: FC = () => {
   };
 
   return (
-    <div className='app-container'>
-      <div className='app'>
-        <div className='input-container'>
-          <div className='input'>
+    <div className='flex px-4 justify-center items-center min-h-screen bg-gradient-to-br from-[#ffece3] to-[#f0b89a]'>
+      <div className='bg-[#4363ec]  rounded-2xl max-w-xs shadow-2xl'>
+        <div className='flex text-white justify-end pt-2 pb-10 pr-2 gap-2'>
+          <span>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke-width='1.5'
+              stroke='currentColor'
+              className='w-5 h-5'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                d='M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z'
+              />
+            </svg>
+          </span>
+          <span>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke-width='1.5'
+              stroke='currentColor'
+              className='w-5 h-5'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                d='M21 10.5h.375c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125H21M4.5 10.5H18V15H4.5v-4.5zM3.75 18h15A2.25 2.25 0 0021 15.75v-6a2.25 2.25 0 00-2.25-2.25h-15A2.25 2.25 0 001.5 9.75v6A2.25 2.25 0 003.75 18z'
+              />
+            </svg>
+          </span>
+        </div>
+        <div className='input-container px-4'>
+          <div>
+            <h2 className='text-3xl mb-4  text-white'>Hello,</h2>
+            <p className='text-3xl text-white'>My Day :</p>
+            <div className='flex justify-between mx-4 mt-8'>
+              <p className='text-gray-300'>
+                <small>Task Name</small>
+              </p>
+              <p className='text-gray-300'>
+                <small>Task time</small>
+              </p>
+            </div>
             <input
+              className='pl-4 py-2 bg-[#2b49c3] rounded-l-full w-4/5 text-white shadow-lg'
               name='task'
               type='text'
               placeholder='Task...'
               value={task}
               onChange={handleChange}
+              required
             />
             <input
+              className='w-1/5 pl-2 bg-[#2b49c3] py-2 rounded-r-full text-white shadow-lg'
               name='deadline'
               type='number'
               value={deadline}
@@ -51,16 +98,23 @@ const App: FC = () => {
               onChange={handleChange}
             />
           </div>
-          <div className='button'>
-            <button onClick={addTask}>Add Task</button>
+          <div className='flex justify-end'>
+            <button
+              className='py-1 px-3 my-2 rounded-full text-white bg-[#ff7f54]'
+              onClick={addTask}
+            >
+              Add Task
+            </button>
           </div>
         </div>
-        <div className='todo'>
-          {todoList.map((task: ITask, key: number) => {
-            return (
-              <TodoTask key={key} task={task} completeTask={completeTask} />
-            );
-          })}
+        <div className='min-h-[100px] mt-2 rounded-t-3xl rounded-b-2xl bg-white'>
+          <div className='px-6 py-6 w-full'>
+            {todoList.map((task: ITask, key: number) => {
+              return (
+                <TodoTask key={key} task={task} completeTask={completeTask} />
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
--- src/TodoTask.tsx
+++ src/TodoTask.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ITask } from "./Interfaces/Interfaces";
 
 interface Props {
@@ -6,14 +7,63 @@ interface Props {
 }
 
 const TodoTask = ({ task, completeTask }: Props) => {
+  const [done, setDone] = useState<boolean>(false);
+  const handleCheck = () => {
+    setDone(!done);
+  };
   return (
-    <div className='task'>
-      <div className='content'>
-        <p>{task.taskName}</p>
-        <p>{task.deadline}</p>
+    <div className='flex justify-between bg-slate-300 mb-2 rounded shadow-md'>
+      <div className='flex justify-between w-2/3 px-4 py-1 font-semibold'>
+        <p
+          className={`${done && "text-gray-500 decoration-slice line-through"}`}
+        >
+          {task.taskName}
+        </p>
+        <p
+          className={`${done && "text-gray-500 decoration-slice line-through"}`}
+        >
+          {task.deadline}
+        </p>
       </div>
-      <div className='dlt-btn'>
-        <button onClick={() => completeTask(task.taskName)}>X</button>
+      <div className='dlt-btn w-1/3 flex'>
+        <button
+          onClick={handleCheck}
+          className='py-1 bg-[#5472ec] w-full rounded-l text-white block h-full'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            className='w-full h-6'
+          >
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M4.5 12.75l6 6 9-13.5'
+            />
+          </svg>
+        </button>
+        <button
+          className='py-1 bg-[#df4136] w-full rounded-r text-white block h-full'
+          onClick={() => completeTask(task.taskName)}
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            className='w-full h-6'
+          >
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
